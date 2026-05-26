fbc634ef371cd1c4448587b27e342d78f2da980b
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2026-05-26 10:15:30 -0400

    initial setup for students

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
+// the setup & draw functions are 'reserved functions' in p5js, meaning p5js uses it internally. notice there are no function calls in this script, yet the code runs in the page! (p5 calls these automatically)
 function setup() {
   createCanvas(400, 400);
 }
 function draw() {
-  background(220);
+  background("aqua");
 }
